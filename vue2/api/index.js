5bbf804563a125f32a8196919ea2c575243fa8ab
telanx <1241818518@qq.com>
2022-01-01 23:38:04 +0800

    test

--- vue2/api/index.js
+++ vue2/api/index.js
@@ -2,10 +2,20 @@ const app = require('express')();
 const { v4 } = require('uuid');
 
 app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+  if (req.query.url) {
+      var url = req.query.url;
+      fetch({url, method: 'GET'}).then((err, res) => {
+          res.json(res);
+      }).catch(err => {
+          res.end('出错了:'+JSON.stringify(err));
+      });
+
+  } else {
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+    res.end(`404 Not Found<br/><hr/>Nginx1.2`);
+  }
+  
 });
 
 app.get('/api/item/:slug', (req, res) => {
